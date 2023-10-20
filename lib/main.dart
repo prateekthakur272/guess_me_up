8acafefc06172bfa5e9ecb20f0de7cf9a0e114bb
Prateek Thakur <prateekthakur272@gmail.com>
2023-10-20 13:40:42 +0530

    adds home screen and game screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,32 @@ class App extends StatelessWidget {
       title: 'Guess Me Up',
       home: const HomeScreen(),
       theme: ThemeData(
-        useMaterial3: true,
-      ),
+          useMaterial3: true,
+          colorScheme: ColorScheme(
+              brightness: Brightness.light,
+              primary: Colors.green.shade400,
+              onPrimary: Colors.black,
+              secondary: Colors.black,
+              onSecondary: Colors.white,
+              error: Colors.red.shade300,
+              onError: Colors.white,
+              background: Colors.white,
+              onBackground: Colors.black,
+              surface: Colors.white,
+              onSurface: Colors.black),
+          appBarTheme: const AppBarTheme(
+              foregroundColor: Colors.black,
+              titleTextStyle: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black)),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+                  foregroundColor: const MaterialStatePropertyAll(Colors.white),
+                  backgroundColor:
+                      MaterialStatePropertyAll(Colors.green.shade400)))),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:guess_me_up/screens/game_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Guess Me Up')));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Guess ME Up'),
+        ),
+        body: Center(
+            child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Guess the number in least time,\nNumber is between 0-100,\nDon\'t worry we are here to help you.',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GameScreen()));
+                  },
+                  child: const Text('Start Game'))
+            ],
+          ),
+        )));
   }
 }
